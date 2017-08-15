41240340ddc11980116a59180425d7a5363bc317
SwarupPattnaik <swarup.pattnaik@evry.com>
2017-08-15 18:02:39 +0530

    Initial Commit

---
+++ ObjectiveC_Playground/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjectiveC_Playground
+//
+//  Created by Swarup on 8/15/17.
+//  Copyright © 2017 Swarup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
